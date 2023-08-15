8e3eaa0ecc6a2921abfbb1af6ef8f498159ada38
nicknamed <nicknamed19@gmail.com>
2023-08-15 14:13:49 -0500

    Se agrega la vista del menu mobil y su funcionalidad

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 //VARIABLES
-const menuEmail = document.querySelector('.user')
+const menuEmail = document.querySelector('.user-desktop')
 const desktopMenu = document.querySelector('.desktop-menu');
+const burgerIcon = document.querySelector('.icon-menu')
+const mobilMenu = document.querySelector('.menu-mobil')
 
 menuEmail.addEventListener('click', toggleDesktopMenu)
+burgerIcon.addEventListener('click', toggleMobilMenu)
 
 function toggleDesktopMenu() {
     desktopMenu.classList.toggle('inactivate')
+}
+
+function toggleMobilMenu() {
+    mobilMenu.classList.toggle('inactivate')
 }
